529ffe2942b3d2a89e4800d0e8e7eaaef93ca9fb
huangzhe <huangzhe@jbufa.com>
2024-07-05 09:04:26 +0800

    add class ParserV0CSV for data v2

--- demos/demo_parser.py
+++ demos/demo_parser.py
@@ -10,7 +10,10 @@ def parse_args():
     # parser.add_argument('--addr', default='/media/manu/data/cable/data_v0/neg')
     # parser.add_argument('--db_type', default='DataV0')
     # parser.add_argument('--addr', default='/media/manu/data/cable/data_v1/runtime/6号线缆/a）6号 定温报警.CSV')
-    parser.add_argument('--addr', default='/media/manu/data/cable/data_v1/runtime')
+    # parser.add_argument('--addr', default='/media/manu/data/cable/data_v1/runtime')
+    # parser.add_argument('--db_type', default='DataV1')
+    # parser.add_argument('--dir_plot_save', default='/home/manu/tmp/cable_demo_parser_save')
+    parser.add_argument('--addr', default='/media/manu/data/cable/data_v2/隧道火灾实验数据/３、隧道侧道火灾实验/6.25隧道侧壁 2L汽油 未报警/200米 20240625.CSV')
     parser.add_argument('--db_type', default='DataV1')
     parser.add_argument('--dir_plot_save', default='/home/manu/tmp/cable_demo_parser_save')
     return parser.parse_args()
@@ -20,7 +23,8 @@ def run(args):
     logging.info(args)
     # parser = ParserV0(db_type=args.db_type, addr_in=args.addr, dir_plot_save=args.dir_plot_save)
     # parser = ParserV1(db_type=args.db_type, addr_in=args.addr, dir_plot_save=args.dir_plot_save)
-    parser = ParserV1CSV(db_type=args.db_type, addr_in=args.addr, dir_plot_save=args.dir_plot_save)
+    parser = ParserV0CSV(db_type=args.db_type, addr_in=args.addr, dir_plot_save=args.dir_plot_save)
+    # parser = ParserV1CSV(db_type=args.db_type, addr_in=args.addr, dir_plot_save=args.dir_plot_save)
     parser.parse()
 
 
--- parsers/parser.py
+++ parsers/parser.py
@@ -33,6 +33,24 @@ class ParserV0(ParserBase):
         db_obj.plot(dir_save=self.dir_plot_save, show=True, save_name_prefix=_name)
 
 
+class ParserV0CSV(ParserBase):
+    """
+    format: <path>
+    """
+
+    def __init__(self, db_type, addr_in, dir_plot_save):
+        super().__init__(db_type, dir_plot_save)
+        self.path_in = addr_in
+
+    def parse(self):
+        _basename = os.path.basename(self.path_in)
+        _name, _ = os.path.splitext(_basename)
+        logging.info(self.path_in)
+        db_obj = eval(self.db_type)(self.path_in)
+        db_obj.load()
+        db_obj.plot(dir_save=self.dir_plot_save, show=True, save_name_prefix=_name)
+
+
 class ParserV1(ParserBase):
     """
     format: dir/<paths>
